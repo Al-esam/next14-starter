085e35b97d6cef37ae18a14b002a7b0b1e2fd5f6
@lesam625 <maselazwelethu@gmail.com>
2024-01-08 16:21:39 +0200

    still Shell

--- src/component/Navbar/links/Links.jsx
+++ src/component/Navbar/links/Links.jsx
@@ -1,32 +1,36 @@
-import Link from "next/link"
-import Styles from "./links.module.css"
+"use client";
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink"; 
+
 const Links = () => {
     const links = [
-        {
-            title: "Home",
-            path:"/",
-        },
-        {
-            title: "About",
-            path:"/about",
-        },
-        {
-            title: "Contact", 
-            path:"/contact",
-        },
-        {
-            title: "Blog",
-            path:"/blog",
-        },
-    ]
-    
-  return (
-    <div className={Styles.links}>
-        {links.map((link=>(
-            <Link href={link.path} key={link.title} >{link.title}</Link>
-        )))}
-    </div>
-  )
-}
+    {
+        title: "Homepage",
+        path: "/",
+    },
+    {
+        title: "About",
+        path: "/about",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+    },
+    {
+        title: "Blog",
+        path: "/blog",
+    },
+    ];
 
-export default Links
+    return (
+        
+        <div className={styles.links}>
+            {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+            ))}
+
+        </div>
+    )
+};
+
+export default Links;
